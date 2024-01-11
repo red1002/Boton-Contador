da0bcc456cdd03ebf234ca88577cb76915063681
Edgar <lemusedgar916@gmail.com>
2024-01-11 17:02:09 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const boton = document.getElementById('boton');
+const valor  = document.getElementById('valor');
+let counter = 0;
+
+boton.addEventListener('click', () =>{
+  counter++;
+  boton.innerText = `Counter: ${counter}`;
+  valor.innerText = counter;
+  console.log(counter);
+})
+
